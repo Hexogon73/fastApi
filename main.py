aba74705a9b20bba97f6aa10152dabe7c4fdf119
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2022-06-17 14:21:09 +0400

    added more examples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Form
 
 app = FastAPI()
 
@@ -6,3 +7,21 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello world"}
+
+
+@app.get("/users/{user_id}")
+async def user(user_id: str):
+    return {"user_id": user_id}
+
+
+users = ["Spike", "Jet", "Ed", "Faye", "Ein"]
+
+
+@app.get("/users")
+async def users_(start: int = 0, limit: int = 10):
+    return users[start:start + limit]
+
+
+@app.post("/lookup")
+async def userlookup(username: str = Form(...), user_id: str = Form("")):
+    return {"username": username, "user_id": user_id}
