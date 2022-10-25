9b85bf45dc652e585c9585075e6451ec3f9a7514
sergey.lukyanov <sergey.lukyanov@simbirsoft.com>
2022-10-25 13:57:34 +0400

    updated methods

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ async def root():
     return {"greeting": "Hello world"}
 
 
-@app.get("/users/{user_id}")
-async def user(user_id: str):
-    return {"user_id": user_id}
-
-
 users = ["Spike", "Jet", "Ed", "Faye", "Ein"]
 
 
@@ -22,6 +17,11 @@ async def users_(start: int = 0, limit: int = 10):
     return users[start:start + limit]
 
 
+@app.get("/users/{user_id}")
+async def user(user_id: str):
+    return {"user_id": user_id, "name": users[int(user_id)]}
+
+
 @app.post("/lookup")
-async def userlookup(username: str = Form(...), user_id: str = Form("")):
+async def user_lookup(username: str = Form(...), user_id: str = Form("")):
     return {"username": username, "user_id": user_id}
